8ac4aa58560c40045eef681137b6d859b9b712aa
Heejoon Kang <haseo.hackers@gmail.com>
2019-06-10 22:36:40 +0900

    Updates

---
+++ precache-manifest.28e0138629e11b135ab327efeab02bbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8683c5b2e2227474da3ec8ed29975256",
+    "url": "/jerry-video/index.html"
+  },
+  {
+    "revision": "c8f319fc4aa88d1056fc",
+    "url": "/jerry-video/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/jerry-video/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c8f319fc4aa88d1056fc",
+    "url": "/jerry-video/static/js/main.8481124f.chunk.js"
+  },
+  {
+    "revision": "2d80d1fd06b2b6bae7a6",
+    "url": "/jerry-video/static/js/runtime~main.81437d05.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jerry-video/static/media/logo.5d5d9eef.svg"
+  }
+]);
