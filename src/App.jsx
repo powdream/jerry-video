6dcf393bfdb7b29901e41c79bd1cfe989e5e51f3
Heejoon Kang <haseo.hackers@gmail.com>
2019-06-16 17:08:10 +0900

    First use of class

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.name
+    };
+  }
+
+  componentDidMount() {
+    console.log("Hello, world");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.jsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+          <p>Trying with class App: {this.state.name}, {this.props.id}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
