92a63e13c86b80227971619d950b4e2df7dbdef0
Heejoon Kang <haseo.hackers@gmail.com>
2019-06-29 18:37:11 +0900

    Parameterize

--- src/page/TopList.jsx
+++ src/page/TopList.jsx
@@ -3,57 +3,41 @@ import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import Progress from '../component/Progress';
 
-const inProgressTabs = () => {
-  return (
-    <Tabs defaultActiveKey="ENTERTAINMENT" transition={false} id="toplist-category-tabs">
-      <Tab eventKey="DRAMA" title="드라마">
-        <Progress isVisible={true} />
-      </Tab>
-      <Tab eventKey="ENTERTAINMENT" title="오락">
-        <Progress isVisible={true} />
-      </Tab>
-      <Tab eventKey="DOCUMENTARY" title="다큐">
-        <Progress isVisible={true} />
-      </Tab>
-      <Tab eventKey="EVENT" title="시사">
-        <Progress isVisible={true} />
-      </Tab>
-      <Tab eventKey="NEWS" title="뉴스,스포츠">
-        <Progress isVisible={true} />
-      </Tab>
-    </Tabs>
-  );
-};
+const categories = [
+  { eventKey: "DRAMA", title: "드라마" },
+  { eventKey: "ENTERTAINMENT", title: "오락" },
+  { eventKey: "DOCUMENTARY", title: "다큐" },
+  { eventKey: "EVENT", title: "시사" },
+  { eventKey: "NEWS", title: "뉴스,스포츠" },
+];
 
-const loadedCategoryTabs = (toplist) => {
+const renderTopList = (toplist, defaultActiveKey, tabContentRenderer) => {
+  const tabList = categories.map(({ eventKey, title }) => (
+    <Tab eventKey={eventKey} title={title} key={eventKey}>
+      {tabContentRenderer(toplist == null ? null : toplist[eventKey])}
+    </Tab>
+  ));
   return (
-    <Tabs defaultActiveKey="ENTERTAINMENT" transition={false}>
-      <Tab eventKey="DRAMA" title="드라마">
-        {toplist.DRAMA[0].title}
-      </Tab>
-      <Tab eventKey="ENTERTAINMENT" title="오락">
-        {toplist.ENTERTAINMENT[0].title}
-      </Tab>
-      <Tab eventKey="DOCUMENTARY" title="다큐">
-        {toplist.DOCUMENTARY[0].title}
-      </Tab>
-      <Tab eventKey="EVENT" title="시사">
-        {toplist.EVENT[0].title}
-      </Tab>
-      <Tab eventKey="NEWS" title="뉴스,스포츠">
-        {toplist.NEWS[0].title}
-      </Tab>
+    <Tabs defaultActiveKey={defaultActiveKey} transition={false}>
+      {tabList}
     </Tabs>
   );
-};
+}
 
 const TopList = ({ topListStatus }) => {
+  const toplist = topListStatus.toplist;
   if (!topListStatus.isLoaded) {
-    return inProgressTabs();
+    return renderTopList(toplist, "ENTERTAINMENT", () => (
+      <Progress isVisible={true} />
+    ));
   } else if (!topListStatus.error) {
-    return loadedCategoryTabs(topListStatus.toplist);
+    return renderTopList(toplist, "ENTERTAINMENT", (channel) => (
+      <div>{channel[0].title}</div>
+    ));
   } else {
-    return <div>{topListStatus.error}</div>;
+    return renderTopList(toplist, "ENTERTAINMENT", () => (
+      <div>{topListStatus.error}</div>
+    ));
   }
 };
 
