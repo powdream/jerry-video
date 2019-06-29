6814de8bb733e1662d52dcf9e1838f24f517e6c1
Heejoon Kang <haseo.hackers@gmail.com>
2019-06-29 20:46:04 +0900

    light/dark

--- src/component/Channel.jsx
+++ src/component/Channel.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Program from './Program';
 
+const isEven = (index) => index % 2 === 0;
+
+const getVariant = (index) => {
+  if (isEven(index)) {
+    return "light";
+  } else {
+    return "dark";
+  }
+};
+
 const Channel = ({ channel }) => {
-  const programList = channel.map((program) => (
-    <ListGroup.Item key={program.uniqueId}>
+  const programList = channel.map((program, index) => (
+    <ListGroup.Item key={program.uniqueId} variant={getVariant(index)}>
       <Program program={program} />
     </ListGroup.Item>
   ));
